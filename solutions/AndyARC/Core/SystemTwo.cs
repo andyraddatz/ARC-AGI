64d6f797a91f2da3a18319e2025d272faf9ec7a2
andy <andyraddatz@gmail.com>
2024-08-03 11:38:43 -0500

    Expand SystemTwo to prep for compound functions

--- solutions/AndyARC/Core/SystemTwo.cs
+++ solutions/AndyARC/Core/SystemTwo.cs
@@ -4,49 +4,86 @@ namespace AndyARC.Core;
 
 public static class SystemTwo
 {
-    public static (int, int) SolvePuzzles(string[] puzzleFiles)
+    public static (int, int) SolvePuzzles(string[] puzzleFilePaths)
     {
+        Console.ForegroundColor = default;
         // todo: lots more actions
-        var actions = SystemOne.Actions;
-        var tests = 0;
+        var puzzles = 0;
         var wins = 0;
-        foreach (var filePath in puzzleFiles)
+        foreach (var filePath in puzzleFilePaths)
         {
             var puz = JsonSerializer.Deserialize<Puzzle>(File.ReadAllText(filePath))
                 ?? throw new Exception($"Failed to load puzzle from {filePath}");
 
-            // training samples
-            var taskNumber = 0;
-            foreach (var t in puz.Train)
-            {
-                foreach (var action in actions)
-                {
-                    var modified = action(t.Input);
-
-                    if (IsMatch(t.Output, modified))
-                        Console.WriteLine($"Train WIN: {Path.GetFileName(filePath)} task {taskNumber} - {action.Method.Name}");
-                }
-                taskNumber++;
-            }
-
-            taskNumber = 0;
+            var action = GetAction(puz, Path.GetFileName(filePath));
+            var win = false;
             foreach (var t in puz.Test)
             {
-                foreach (var action in actions)
-                {
-                    // test samples
-                    var modified = action(t.Input);
-                    if (IsMatch(t.Output, modified))
-                    {
-                        wins++;
-                        Console.WriteLine($"TEST WIN: {Path.GetFileName(filePath)} task {taskNumber} - {action.Method.Name}");
-                    }
-                }
-                taskNumber++;
-                tests++;
+                var modified = action(t.Input);
+                win = IsMatch(t.Output, modified);
             }
+            if (win)
+            {
+                wins++;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"TEST WIN: {Path.GetFileName(filePath)}", ConsoleColor.Green);
+                Console.ForegroundColor = default;
+            }
+            puzzles++;
         }
-        return (tests, wins);
+        return (puzzles, wins);
+    }
+
+    private static Func<int[][], int[][]> GetAction(Puzzle puz, string puzName)
+    {
+
+        Func<int[][], int[][]> compoundAction = (x) =>
+        {
+            var modified = x;
+            foreach (var action in SystemOne.Actions)
+            {
+                modified = action(modified);
+            }
+            return modified;
+        };
+        var allFuncs = SystemOne.Actions;
+        allFuncs = allFuncs.Append(compoundAction);
+
+        foreach (var action in allFuncs)
+        {
+            // if we find any action that works for all training tasks
+            var candidateFound = true;
+            foreach (var t in puz.Train)
+            {
+                var modified = action(t.Input);
+                if (IsMatch(t.Output, modified))
+                {
+                    Console.WriteLine($"Training match: {puzName} - {action.Method.Name}");
+                    candidateFound = candidateFound && IsMatch(t.Output, modified);
+                }
+                candidateFound = candidateFound && IsMatch(t.Output, modified);
+            }
+
+            // AND is plausible for the test tasks, we're done
+            candidateFound = candidateFound && IsPlausible(action, puz.Test);
+            if (candidateFound) return action;
+        }
+
+        return _ => _;
+    }
+
+    private static bool IsPlausible(Func<int[][], int[][]> action, IEnumerable<ARCSample> test)
+    {
+        var isPlausible = false;
+        foreach (var t in test)
+        {
+            // todo: define plausibility
+            var modified = action(t.Input);
+            // output grid is within possible bounds
+            isPlausible = modified.Length <= 30 && modified.All(row => row.Length <= 30);
+            // input grid has features expected by the action
+        }
+        return isPlausible;
     }
 
     static bool IsMatch(int[][] actual, int[][] proposed)
--- solutions/AndyARC/Program.cs
+++ solutions/AndyARC/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System.Text.Json;
 using AndyARC.Core;
 
+Console.ForegroundColor = ConsoleColor.White;
+
 // ARC-AGI solver
 Console.WriteLine("ARC-AGI Solver");
 
@@ -8,10 +10,13 @@ Console.WriteLine("ARC-AGI Solver");
 Console.WriteLine("Training set...");
 var trainingFiles = Directory.GetFiles("../../data/training", "*.json");
 var (tests, wins) = SystemTwo.SolvePuzzles(trainingFiles);
+Console.ForegroundColor = ConsoleColor.White;
 Console.WriteLine($"Training set Tests: {tests}, Wins: {wins}");
 
 // eval
 var evalFiles = Directory.GetFiles("../../data/evaluation", "*.json");
+Console.ForegroundColor = ConsoleColor.White;
 Console.WriteLine("Evaluation set...");
 (tests, wins) = SystemTwo.SolvePuzzles(evalFiles);
+Console.ForegroundColor = ConsoleColor.White;
 Console.WriteLine($"Evaluation set Tests: {tests}, Wins: {wins}");
